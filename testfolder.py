033a73ecc8389b05c54ab2e28ea635e4837c4e47
farzanehashemabadi <71126340+farzanehashemabadi@users.noreply.github.com>
2021-02-08 20:02:45 +0330

    Add files via upload

--- testfolder.py
+++ testfolder.py
@@ -1,10 +1,10 @@
 import os
-import arcpy
-#import shapefile
 import glob
+import arcpy
 from arcpy import env
 
-    
+
+
 # setting my source and target geodatabase 
 base_dir = os.path.dirname(__file__)
 
@@ -13,60 +13,69 @@ clippers_dir = r"C:\Users\Farzane\Desktop\Master97\Gnome modeling\Postprocessing
 clippers = glob.glob(clippers_dir + '/*.shp')
 
 # Iterate in base path and work with folders
-def list_files(base_dir):                                                                                                  
-    r = []                                                                                                            
-    subdirs = [x[0] for x in os.walk(base_dir)]                                                                            
-    for subdir in subdirs:                                                                                            
-        files = os.walk(subdir).next()[2]                                                                             
-        src_dir = files
-        des_dir = os.path.join (src_dir, "clipped")
-        if not os.path.exists(des_dir):
-            os.mkdir(des_dir)
-                
-        # set a workspace    
-        arcpy.env.workspace = src_dir
-        fclist = arcpy.ListFeatureClasses()
-        
-        # make list of Grids
-        Grid_list = [0 for i in range(len(clippers))]   
 
-        ###########################################
-        for fc in fclist:
-            src_file_dir, src_file_name = src_dir, fc
-            for index, cfs in enumerate(clippers):
-                clipper_dir, clipper_name = os.path.split(cfs)
-                clipper_name = src_file_name.split('.')[0] + "_" + clipper_name 
-                output_dir = os.path.join(des_dir , clipper_name)
-                clipped = arcpy.Clip_analysis(fc,cfs,output_dir)
-                  
-                #Calculate zigmaFID for each Grid in a Scenario
-                shps = glob.glob (des_dir + '/*.shp')
-                  
-                for shp in shps:
-                    fid = set(row[0] for row in arcpy.da.SearchCursor(shp, "FID"))
-                    count = len (fid)
-                    #print (count)
-                    Grid_list[index] += count
-                    print (Grid_list[index])  
+positions = [(49.68, 29.94, 0),
+             (49.71, 30.12, 0),
+             (49.58, 29.53, 0),
+             (49.60, 29.88, 0),
+             (49.66, 29.01, 0),
+             (50.44, 28.96, 0),
+             (49.80, 28.27, 0),
+             (54.06, 25.75, 0),
+             (54.79, 25.69, 0),
+             (54.72, 25.89, 0),
+             (54.02, 25.54, 0),
+             (54.31, 25.48, 0),
+             (52.93, 25.53, 0),
+             (53.12, 25.96, 0),
+             (52.78, 25.92, 0),
+             (52.53, 26.31, 0)]
 
-                                                                                         
+seasons = ['Winter', 'Summer']
+
+src_dirs = [os.path.join(base_dir, 'Result {}_{}'.format(spill_num, positions)) for spill_num, positions in zip(range(1,101),positions)]
 
 
 
-
-
-
-
+for src_dir in src_dirs:
+    des_dir = os.path.join (src_dir, "clipped")
+    if not os.path.exists(des_dir):
+        os.mkdir(des_dir)
     
+    #set a workspace    
+    arcpy.env.workspace = src_dir
+
+    fclist = arcpy.ListFeatureClasses()
+
+    # make list of Grids
+    Grid_list = [0 for i in range(len(clippers))]
+
+
+    ###########################################          
+
+    for fc in fclist:
+        src_file_dir, src_file_name = src_dir, fc
+        for index, cfs in enumerate(clippers):
+
+            clipper_dir, clipper_name = os.path.split(cfs)
+            clipper_name = src_file_name.split('.')[0] + "_" + clipper_name 
+            output_dir = os.path.join(des_dir , clipper_name)
+            clipped = arcpy.Clip_analysis(fc,cfs,output_dir)
+            
+            
+    #Calculate zigmaFID for each Grid in a Scenario
+    shps = glob.glob (des_dir + '/*.shp')
+
+    for shp in shps:
+        fid = set(row[0] for row in arcpy.da.SearchCursor(shp, "FID"))
+        count = len (fid)
+        Grid_list[index] += count
+    print (Grid_list[index])
+        
+            
+
+                
 
 
 
 
-
-
-
-
-
-
-                                                                                                                      
-                              
