9958c68b89470945dece1e15f307b009ba904de9
farzanehashemabadi <71126340+farzanehashemabadi@users.noreply.github.com>
2021-02-15 23:34:49 +0330

    Add files via upload

--- ProcessShell.ps1
+++ ProcessShell.ps1
@@ -1,6 +1,10 @@
-$a = Start-Process - C:/Python27/ArcGIS10.8/python -ArgumentList ("testprocess.py","1")
-$b = Start-Process - C:/Python27/ArcGIS10.8/python -ArgumentList ("testprocess.py","2")
-
+   
+$a = Start-Process -NoNewWindow C:\Python27\ArcGIS10.8\python -ArgumentList ("testprocess.py","1")
+$b = Start-Process -NoNewWindow C:\Python27\ArcGIS10.8\python -ArgumentList ("testprocess.py","2")
+$c = Start-Process -NoNewWindow C:\Python27\ArcGIS10.8\python -ArgumentList ("testprocess.py","3")
+$d = Start-Process -NoNewWindow C:\Python27\ArcGIS10.8\python -ArgumentList ("testprocess.py","4")
 
 $a | wait-process
 $b | wait-process
+$c | wait-process
+$d | wait-process
